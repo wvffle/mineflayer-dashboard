3bf1be669c73f8b9c38e444f5dd7b45691da0379
Kasper Seweryn <github@wvffle.net>
2020-09-11 01:52:19 +0200

    Fix #6

--- src/input-manager.js
+++ src/input-manager.js
@@ -25,14 +25,9 @@ input._listener = async function (ch, key) {
   }
 
   if (key.name === 'tab') {
+    const right = value.slice(cursor)
     const res = await mode.complete(value.slice(0, cursor), key.shift ? -1 : 1)
-    this.value = value.slice(0, cursor) + res
-
-    if (offset < 0) {
-      while (offset++) {
-        program._write('\x1b[1C')
-      }
-    }
+    this.value = value.slice(0, cursor) + res + right
 
     return this.screen.render()
   } else if (mode.resetCompletion()) {
